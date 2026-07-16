1d83b7937cfcdba7a56a0207248d3091215d14a4
Mark Simmons <msimmons431@gmail.com>
2026-07-16 10:44:55 -0400

    Adding new pytest features

---
+++ testing_framework/test_messin/conftest.py
@@ -0,0 +1,11 @@
+import pytest, random, string, json
+
+@pytest.fixture
+def return_a_random_string():
+    return "".join(random.choices(string.ascii_letters + string.digits, k=10))
+
+@pytest.fixture
+def return_some_json_data() -> dict:
+    with open("files/readfile","r") as f:
+        data = json.load(f)
+    return data[0]
