afbb9daff1ecd289dabd32f0706a37b8e671e5b5
Mark Simmons <msimmons431@gmail.com>
2026-07-28 12:46:35 -0400

    Adding GH actions for pytest

---
+++ ui/POM/test_home_page_obj_model.py
@@ -0,0 +1,39 @@
+import playwright
+
+
+class HomePageObjModel:
+    def __init__(self,page) -> None:
+        self.page = page
+        self.home_page = self.page.get_by_role("link", name="Home")
+        self.email_text = self.page.get_by_text("Enter your email address to")
+
+class DevelopmentPageObjModel():
+    def __init__(self,page) -> None:
+        self.page = page
+        self.dev_page = page.get_by_role("link", name="Development", exact=True)
+
+class LoginPageObjModel():
+    def __init__(self,username,password,page) -> None:
+        self.username = username
+        self.password = password
+        self.page = page
+
+    def login_and_return_page(self) -> playwright:
+        self.page.get_by_test_id("handle-button").click()
+        self.page.get_by_test_id("signUp.switchToSignUp").click()
+        self.page.get_by_role("button", name="Log in with Email").click()
+        self.page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").click()
+        self.page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").fill(self.username)
+        self.page.get_by_role("textbox", name="Password").click()
+        self.page.get_by_role("textbox", name="Password").fill(self.password)
+        self.page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
+        self.page.locator("body").press("Escape")
+        return self.page
+
+class KubePage():
+    def __init__(self,kubepage):
+        self.kubepage = kubepage
+
+    def return_kube_page(self) -> playwright:
+        return self.kubepage
+
