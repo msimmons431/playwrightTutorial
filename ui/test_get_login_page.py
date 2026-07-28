7ababa06dda96887fae904285d7c7104e0199f80
Mark Simmons <msimmons431@gmail.com>
2026-07-28 18:58:08 -0400

    trying breaking out of POM 1

--- ui/test_get_login_page.py
+++ ui/test_get_login_page.py
@@ -4,7 +4,7 @@ from POM.test_home_page_obj_model import LoginPageObjModel
 from POM.test_home_page_obj_model import LoginPageObjModel as LoginPageObj
 
 
-def test_get_login_page_obj(playwright: Playwright):
+async def test_get_login_page_obj(playwright: Playwright):
     username="symon.storozhenko@gmail.com"
     password="test123"
     browser = playwright.chromium.launch(headless=True)
@@ -13,7 +13,10 @@ def test_get_login_page_obj(playwright: Playwright):
     page.goto("https://symonstorozhenko.wixsite.com/website-1/")
     #login_page_obj = LoginPageObj(username,password,page)
     #login_page = login_page_obj.login_and_return_page()
-    page.get_by_test_id("handle-button").click()
+    #page.get_by_test_id("handle-button").click()
+    button = page.get_by_test_id("handle-button")
+    await expect(button).to_be_visible(timeout=15000)
+    await button.click()
     page.get_by_test_id("signUp.switchToSignUp").click()
     page.get_by_role("button", name="Log in with Email").click()
     page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").click()
