03a78e4dd40b1725559f1156868b523bc1dd5ca1
Mark Simmons <msimmons431@gmail.com>
2026-07-28 13:08:12 -0400

    Removed headless false as the ubuntu sys is not running Xterms

--- ui/test_get_login_page.py
+++ ui/test_get_login_page.py
@@ -6,7 +6,7 @@ from POM.test_home_page_obj_model import LoginPageObjModel as LoginPageObj
 def test_get_login_page_obj(playwright: Playwright):
     username="symon.storozhenko@gmail.com"
     password="test123"
-    browser = playwright.chromium.launch(headless=False, slow_mo=999)
+    browser = playwright.chromium.launch(headless=True)
     page = browser.new_page()
     page.goto("https://symonstorozhenko.wixsite.com/website-1/")
     login_page_obj = LoginPageObj(username,password,page)
