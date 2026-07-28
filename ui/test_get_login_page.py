223888d965df57f83358bafb6370ed8e8f7bfff5
Mark Simmons <msimmons431@gmail.com>
2026-07-28 14:48:16 -0400

    page issues

--- ui/test_get_login_page.py
+++ ui/test_get_login_page.py
@@ -8,7 +8,7 @@ def test_get_login_page_obj(playwright: Playwright):
     password="test123"
     browser = playwright.chromium.launch(headless=False,slow_mo=999)
     page = browser.new_page()
-    page = page.goto("https://symonstorozhenko.wixsite.com/website-1/",timeout=90000)
+    page.goto("https://symonstorozhenko.wixsite.com/website-1/",timeout=90000)
     login_page_obj = LoginPageObj(username,password,page)
     login_page = login_page_obj.login_and_return_page()
     login_page.goto("https://symonstorozhenko.wixsite.com/website-1/account/my-account")
