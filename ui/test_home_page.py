a18c28de087e3530c5b1ac3b23df481042294e6c
Mark Simmons <msimmons431@gmail.com>
2026-07-28 17:00:23 -0400

    Playwright working on Local system

--- ui/test_home_page.py
+++ ui/test_home_page.py
@@ -7,7 +7,7 @@ from POM.test_home_page_obj_model import DevelopmentPageObjModel, HomePageObjMod
 
 
 def test_dev_page_obj_model(playwright: Playwright):
-    browser = playwright.chromium.launch()
+    browser = playwright.chromium.launch(headless=False, slow_mo=999)
     page = browser.new_page()
     page.goto("https://automationpanda.com/")
     dev_page_obj = DevelopmentPageObjModel(page)
@@ -28,7 +28,7 @@ def test_dev_page_obj_model(playwright: Playwright):
     print(f"\n\nJOIN TYPE {type(joinwords)}\n")
 
 def home_page_obj_model(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False,slow_mo=999)
+    browser = playwright.chromium.launch(headless=False, slow_mo=999)
     page = browser.new_page()
     page.goto("https://automationpanda.com/")
     home_page_obj = HomePageObjModel(page)
