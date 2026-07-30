532f5b008437218a54a3532efbb28c9b348a4f98
Mark Simmons <msimmons431@gmail.com>
2026-07-30 14:11:33 -0400

    Added new playwright test cases.

---
+++ ui/test_UIvalidations_1.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import Page, expect
+def test_UIValidationDynamicScript(page: Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.get_by_label("Username:").fill("rahulshettyacademy")
+    page.get_by_label("Password:").fill("Learning@830$3mK2")
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
+    page.get_by_role("button", name="Sign In").click()
+    iphoneProduct = page.locator("app-card").filter(has_text="iphone X")
+    iphoneProduct.get_by_role("button").click()
+    NokiaProduct = page.locator("app-card").filter(has_text="Nokia Edge")
+    NokiaProduct.get_by_role("button").click()
+    # expect is like an assertion
+    expect(page.get_by_text("Checkout ( 2 ) (current)")).to_be_visible()
+    page.get_by_text("Checkout").click()
+    expect(page.locator(".media-body")).to_have_count(2)
+
--- ui/test_playwright_basics.py
+++ ui/test_playwright_basics.py
@@ -1,6 +1,7 @@
 # The class import Page has the fixture page. Add that import so the IDE
 # can display the methods in page.xxx
 from playwright.sync_api import Playwright, Page, expect, sync_playwright
+import re,time
 
 # FYI: (playwright) in the function is a playwright pytest fixture.
 def test_playwrightBasics(playwright: Playwright):
@@ -14,8 +15,25 @@ def test_playwrightBasics(playwright: Playwright):
 # It will by default launch a chromium headless mode 1 single context page
 # saving you a few steps as seen in the above test_playwrightBasics function.
 def test_playwrightShortCut(page: Page ):
+    journeypat = re.compile(r'.*Choose Your Learning (Journey).*')
     page.goto("https://rahulshettyacademy.com")
+    page.get_by_role("navigation").get_by_role("link", name="Learning Paths").click()
+    text = page.get_by_text("Choose Your Learning Journey").text_content()
+    jrnymatch = re.match(journeypat,text)
+    print(f"DEBUG: Journey match is {jrnymatch.group(1)}") if jrnymatch.group(1) else print("No Journey?")
+    page.close()
 
+
+def test_coreLocators(page: Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.get_by_label("Username:").fill("rahulshettyacademy")
+    page.get_by_label("Password:").fill("Learning@830$3mK2")
+    #page.get_by_label("Password:").fill("FOO____@830$3mK2")
+    page.get_by_role("combobox").select_option("teach")
+    page.get_by_role("checkbox", name="terms").click()
+    page.get_by_role("button", name="Sign In").click()
+    #expect(page.get_by_text("Incorrect username/password.")).to_be_visible()
+    time.sleep(8)
 
 
 
